2dc2c344f6bd10cbc7bcd1fe64af23b6c75a8b20
murillo <mac0321@bacamarte.cec.ime.usp.br>
2025-03-20 10:30:47 -0300

    lerlero py

--- lerolero.py
+++ lerolero.py
@@ -5,4 +5,12 @@ Gerador de lero-lero.
 Gera frases de efeito sem significado real.
 """
 
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+
 
