1b13305727b94595a03c9f546f160baf912de6ab
murillo <mac0321@bacamarte.cec.ime.usp.br>
2025-03-20 11:02:57 -0300

    fix

--- lerolero.py
+++ lerolero.py
@@ -11,7 +11,7 @@ parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "
 parte2 = ["possui excelente desempenho","oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
 parte3 = ["nas aplicações a que se destina", "em relação às opções disponíveis no mercado", ", provendo ampla vantagem competitiva a seus usuários"]
 
-lingua = int((input("Escolha a língua: 1 - português; 2 - inglês\n"))
+lingua = int(input("Escolha a língua: 1 - português; 2 - inglês\n"))
 
 if lingua == 2:
 	parte1 = []
